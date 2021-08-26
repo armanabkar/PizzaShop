49b455bd326783b1f2080f23ad8be5f9f87c3a67
Arman Abkar <armanabkar@gmail.com>
2021-08-26 23:33:12 +0430

    Initial Commit

---
+++ PizzaShop/Controllers/CartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CartViewController.swift
+//  PizzaShop
+//
+//  Created by Arman Abkar on 8/26/21.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PizzaShop/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaShop
+//
+//  Created by Arman Abkar on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
