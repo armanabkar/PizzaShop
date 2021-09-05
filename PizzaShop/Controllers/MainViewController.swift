5290c33da0d968241548f97848e1b503c54b93fa
Arman Abkar <armanabkar@gmail.com>
2021-09-05 16:25:34 +0430

    add main scren

---
+++ PizzaShop/Controllers/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  PizzaShop
+//
+//  Created by Arman Abkar on 9/5/21.
+//
+
+import UIKit
+import MapKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        showRestaurantLocation()
+    }
+
+    func showRestaurantLocation() {
+        var c = CLLocationCoordinate2D()
+        c.latitude = 34.402341
+        c.longitude = -119.726045
+        
+        let a = MKPointAnnotation()
+        a.coordinate = c
+        a.title = "Pizza Pizza"
+        mapView.addAnnotation(a)
+        
+        let restaurantLocation = CLLocationCoordinate2D(latitude: 34.402341, longitude: -119.726045)
+        mapView.setCenter(restaurantLocation, animated: true)
+        
+        let viewRegion = MKCoordinateRegion(center: c, latitudinalMeters: 1500, longitudinalMeters: 1500)
+        mapView.setRegion(viewRegion, animated: false)
+    }
+    
+}
